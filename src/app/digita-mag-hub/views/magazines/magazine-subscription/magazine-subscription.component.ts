ff6c7db8344e9cd3d064f24237022a668af37ab1
adolfosonchiche <adolfoson36@gamil.com>
2024-03-31 12:07:44 -0600

    MS-35 #comment download magazine

--- src/app/digita-mag-hub/views/magazines/magazine-subscription/magazine-subscription.component.ts
+++ src/app/digita-mag-hub/views/magazines/magazine-subscription/magazine-subscription.component.ts
@@ -114,4 +114,20 @@ export class MagazineSubscriptionComponent implements OnInit {
     void this.router.navigate(['/digital/magazines/subscription/' + this.magazineId + '/view'])
   }
 
+  downloadMagazine(){
+    const byteCharacters = atob(this.magazine.file);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'application/pdf' });
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `${this.magazine.name}.pdf`;
+
+    link.click();
+  }
+
 }
